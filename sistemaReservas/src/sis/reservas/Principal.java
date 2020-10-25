85b41b5129f9f953ec497b3c7a736e94cb354393
Juan Martín <jmaraya97@gmail.com>
2020-10-25 19:18:03 +0100

    Add files via upload

---
+++ sistemaReservas/src/sis/reservas/Principal.java
@@ -0,0 +1,55 @@
+package sis.reservas;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Principal {
+	
+	public static void main(String[] args) throws IOException {		
+		ProcessBuilder builder = new ProcessBuilder(args); //Pasamos por argumentos el comando java -jar C:/Users/jmara/Documents/secundaria.jar que contiene la clase Secundaria y Pistas 
+		builder.redirectErrorStream(true);
+		Process process = builder.start();
+		InputStream out = process.getInputStream();//salida del proceso hijo en la entrada del padre
+		OutputStream in = process.getOutputStream(); //entrada del proceso hijo en la salida del padre
+ 
+		byte[] buffer = new byte[4000];//buffer
+		
+		while (isAlive(process)) {
+			int no = out.available();
+			if (no > 0) {
+				int n = out.read(buffer, 0, Math.min(no, buffer.length));
+				System.out.println(new String(buffer, 0, n));
+			}
+
+			int ni = System.in.available();
+			if (ni > 0) {
+				int n = System.in.read(buffer, 0, Math.min(ni, buffer.length));
+				in.write(buffer, 0, n);
+				in.flush();
+			}
+			
+			try {
+				//Retardo de tiempo de 10 milisegundos, suficiente para que no se pisen los procesos.
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				Thread.currentThread().interrupt();
+			}
+		}
+
+		System.out.println(process.exitValue());
+	}
+	
+	//Metodo que comprueba si el proceso hijo ha terminado. Devuelve falso en caso afirmativo
+	//y en caso negativo devuelve true y lanza una excepcion.
+	public static boolean isAlive(Process p) {
+		try {
+			p.exitValue();
+			return false;
+		} catch (IllegalThreadStateException e) {
+			return true;
+		}
+	}
+
+}
---
+++ sistemaReservas/src/sis/reservas/Secundaria.java
@@ -0,0 +1,91 @@
+package sis.reservas;
+
+//imports
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+//Clase secundaria/hijo que tiene todo el codigo relevante al proceso de alquiler de las pistas
+
+public class Secundaria {
+
+	public static void main(String[] args) {
+		ArrayList<Pistas> lista = new ArrayList<>();
+		lista.add(new Pistas("Uno"));
+		lista.add(new Pistas("Dos"));
+
+		Scanner sc = new Scanner(System.in);
+		String opc = "";
+		while (!opc.equalsIgnoreCase("Salir")) {
+			muestraMenu();
+			opc = sc.next();
+			switch (opc.toLowerCase()) {
+
+			case "alquilar":
+				alquilar(lista);
+				break;
+
+			case "consulta":
+				for (int i = 0; i < lista.size(); i++) {
+					lista.get(i).muestraDatos();
+				}
+				break;
+
+			case "salir":
+				System.out.println("Saliendo...");
+				break;
+
+			default:
+				System.out.println("No entiendo el mensaje");
+				break;
+
+			}
+		}
+	}
+
+	//Metodo que muestra el menu por consola
+	public static void muestraMenu() {
+		System.out.println("****************************");
+		System.out.println("*Aplicación de reserva de pistas de padel.");
+		System.out.println("**********Opciones**********");
+		System.out.println("*Alquilar");
+		System.out.println("*Consulta");
+		System.out.println("*Salir");
+		System.out.println("****************************");
+	}
+
+	//Metodo usado para la comprobación y alquiler de la pista
+	public static void alquilar(ArrayList<Pistas> lista) {
+		System.out.println("Introduzca la pista Uno o Dos");
+		Scanner sc1 = new Scanner(System.in);
+		String pista = sc1.next();
+		String horario = "";
+		boolean enc = false;
+
+		for (int i = 0; i < lista.size() && !enc; i++) {
+			if (lista.get(i).getCodigo().equalsIgnoreCase(pista)) {
+				enc = true;
+				if (enc && !lista.get(i).isAlquilada()) {
+					System.out.println("¿Querria alquilar noche o tarde?");
+					Scanner sc2 = new Scanner(System.in);
+					horario = sc2.next();
+					if (horario.equalsIgnoreCase("noche") || horario.equalsIgnoreCase("tarde")) {
+						lista.get(i).setAlquilada(true);
+						lista.get(i).setHorario(horario);
+						System.out.println("Pista " + pista + " alquilada por la " + horario);
+					} else {
+						System.out.println("El horario indicado no es valido");
+					}
+
+				} else {
+					System.out.println("La pista está alquilada");
+				}
+			}
+		}
+		if (!enc) {
+			System.out.println("La pista indicada no existe");
+		}
+
+	}
+
+}
