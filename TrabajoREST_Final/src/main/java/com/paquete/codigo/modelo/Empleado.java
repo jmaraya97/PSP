88f607008b65de662ac92917e42fa5d97a4b409f
Juan Martín <jmaraya97@gmail.com>
2021-03-01 20:59:51 +0100

    Add files via upload

---
+++ TrabajoREST_Final/src/main/java/com/paquete/codigo/modelo/Empleado.java
@@ -0,0 +1,42 @@
+package com.paquete.codigo.modelo;
+
+
+/**
+ * 
+ * @author Juan Martin
+ * Clase POJO
+ */
+public class Empleado {
+	String id;
+	String nombre;
+	int edad;
+	Departamento departamento;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+	
+	
+}
